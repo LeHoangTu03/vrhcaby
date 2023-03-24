f644e5b9000e2aecaf67b06ad5a32183551bde3e
AKuraksa <andrej.kuraksa@seznam.cz>
2023-03-24 13:49:00 +0100

    Nějaká menší práce

---
+++ Jak pracovat s githubem.md
@@ -0,0 +1,10 @@
+# Základní příkazy
+
+Přidat vše:                 git add . (nebo *)
+Přidat komentář:            git commit -m "komentar ke zmene"
+Update z lokálu na server:  git push
+Update ze serveru na lokál: git pull
+
+## Konfigurace
+git config user.email "email"
+git config user.name "jmeno
--- vrhcaby.py
+++ vrhcaby.py
@@ -1,17 +1,38 @@
-def f(x):
-    return x
+from random import randint
 
-print("Hello World")
+class Hra:  #herní deska
 
-# TADY MAS TY KOMENTARE
-# konfigurace
-# git config user.email "email"
-# git config user.name "jmeno"
+    def __init__(self):
+       pass
+    
+    def hernipole(self):    # modifikovaný zásobník, lze vkládat jen kameny stejných barev
+        pass
 
-# git add
-# git commit -m "komentar ke zmene"
+    def dvojkostka(self):   # vrací seznam možných dvojic či čtveřic
+        prvni_kostka, druha_kostka = randint(1,6), randint(1,6)
+        hod = f"Hod kostkou\nPrvní kostka: {prvni_kostka}\nDruhá kostka: {druha_kostka}"
+        if prvni_kostka == druha_kostka:
+            return hod + "\nHodil si double a proto můžeš táhnout čtyřikrát"
+        else:
+            return hod
 
-# git push
-# git pull
+    def bar(self):  # továrna na herní kameny, s řízenou produkcí
+        pass
 
-print("Dal bych si závitky")
+    def herni_kamen(self):  # s pamětí, kde se postupně nacházel
+        pass
+
+class Hrac(Hra):    # odvozená třída
+    
+    def __init__(self):
+        pass
+
+
+def main():
+    hra = Hra()
+    print(hra.dvojkostka())
+    print("Dal bych si závitky")
+
+
+if __name__ == "__main__":
+    main()
