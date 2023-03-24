bc95a6fec8dd38a0e709abbd1e60635916517c65
Le Hoang Tu <lehoangtu16703@gmail.com>
2023-03-24 12:28:06 +0100

    Přidány komentáře

--- vrhcaby.py
+++ vrhcaby.py
@@ -1,1 +1,12 @@
 print("Hello World")
+
+# TADY MAS TY KOMENTARE
+# konfigurace
+# git config user.email "email"
+# git config user.name "jmeno"
+
+# git add
+# git commit -m "komentar ke zmene"
+
+# git push
+# git pull
